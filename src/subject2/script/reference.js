0ace3267a12708de3ca8352b49bd76a474e27a65
112hyer <143990481+112hyer@users.noreply.github.com>
2023-10-03 04:43:31 +0900

    2차최종업로드

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,16 @@
+const reference = [
+  {
+    author: "KBO",
+    title: "야구 기록법 소개",
+    year: "",
+    sourceTitle: "",
+    link: "https://www.koreabaseball.com/Kbo/AboutKbo/Committee/RecordRaw.aspx",
+  },
+  {
+    author: "박동희",
+    title: "[박동희의 야구탐사] 한국 야구기록의 역사 [1] 야구기록 탄생의 비화",
+    year: "2011",
+    sourceTitle: "",
+    link: "https://m.blog.naver.com/dhp1225/120134679399",
+  },
+];
---
+++ src/subject3/script/reference.js
@@ -0,0 +1,9 @@
+const reference = [
+  {
+    author: "커피박 재자원화 프로젝트",
+    title: "커피박이란?",
+    year: "",
+    sourceTitle: "",
+    link: "http://coffeebak.kr/explanation/",
+  },
+];
